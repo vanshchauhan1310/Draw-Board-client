cf99e24e70c94412a7f1172878ab7401bc31b2e2
vanshchauhan1310 <112659160+vanshchauhan1310@users.noreply.github.com>
2024-12-13 15:19:06 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://draw-board-server.vercel.app", {
+const socket = io("https://draw-board-server.onrender.com/", {
   transports: ['websocket', 'polling'],
   withCredentials: true,
 });
